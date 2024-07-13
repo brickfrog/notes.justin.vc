e92f2d8b9da0617c25805c64830e1558260d2350
Justin <9146678+brickfrog@users.noreply.github.com>
2024-07-12 21:06:30 -0400

    footer aesthetics

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -13,10 +13,6 @@ export default ((opts?: Options) => {
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
-        <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
-        </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
             <li>
@@ -24,6 +20,13 @@ export default ((opts?: Options) => {
             </li>
           ))}
         </ul>
+        <span class="footer-small">
+          {i18n(cfg.locale).components.footer.createdWith}{" "}
+          <a href="https://quartz.jzhao.xyz/">
+            quartz<sup>{version}</sup>
+          </a>{" "}
+          © {year}
+        </span>
       </footer>
     )
   }
