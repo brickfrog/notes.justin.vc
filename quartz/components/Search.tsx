33bc75190c2fbf6b351eb4ddc3293ad7c2ba1693
Justin <9146678+brickfrog@users.noreply.github.com>
2024-07-20 04:18:14 -0400

    search aesthetics

--- quartz/components/Search.tsx
+++ quartz/components/Search.tsx
@@ -20,8 +20,6 @@ export default ((userOpts?: Partial<SearchOptions>) => {
     return (
       <div class={classNames(displayClass, "search")}>
         <div id="search-icon">
-          <p>{i18n(cfg.locale).components.search.title}</p>
-          <div></div>
           <svg
             tabIndex={0}
             aria-labelledby="title desc"
@@ -36,6 +34,21 @@ export default ((userOpts?: Partial<SearchOptions>) => {
               <circle cx="8" cy="8" r="7" />
             </g>
           </svg>
+          <p class="search-title">{i18n(cfg.locale).components.search.title}</p>
+          <div class="search-keys">
+            <kbd class="search-key">
+              <svg width="15" height="15" class="DocSearch-Control-Key-Icon">
+                <path
+                  d="M4.505 4.496h2M5.505 5.496v5M8.216 4.496l.055 5.993M10 7.5c.333.333.5.667.5 1v2M12.326 4.5v5.996M8.384 4.496c1.674 0 2.116 0 2.116 1.5s-.442 1.5-2.116 1.5M3.205 9.303c-.09.448-.277 1.21-1.241 1.203C1 10.5.5 9.513.5 8V7c0-1.57.5-2.5 1.464-2.494.964.006 1.134.598 1.24 1.342M12.553 10.5h1.953"
+                  stroke-width="1.2"
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-linecap="square"
+                ></path>
+              </svg>
+            </kbd>
+            <kbd class="search-key">K</kbd>
+          </div>
         </div>
         <div id="search-container">
           <div id="search-space">
